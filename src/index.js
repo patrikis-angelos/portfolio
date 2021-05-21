5cc5cf11f2c0842e1487164afcd2216fc85f3cd3
patrick-angelos <angelosp07@gmail.com>
2021-05-21 21:49:54 +0300

    Add item creation functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import './reset.css';
 import './styles.css';
-import './images/enigma.png';
-import './images/Tictactoe.png';
+import enigma from './images/enigma.png';
+import tic from './images/Tictactoe.png';
 import move from './dragable';
+import createItem from './display';
 
-const elements = document.getElementsByClassName("dragable")
+const elements = document.getElementsByClassName('dragable');
 for (let i = 0; i < elements.length; i++) {
   move(elements[i]);
 }
+
+const desc = 'EniGma? is a Desktop app where users can create and view different scientific articles. Singed-in users can up-vote articles. The one with the most votes will be on the home page.';
+const links = [
+  {
+    url: 'https://enigma-articles.herokuapp.com/',
+    text: 'See project',
+  },
+  {
+    url: 'https://github.com/patrick-angelos/Enigma',
+    text: 'Github',
+  },
+];
+const item1 = createItem('a', 'a', ['a'], 'a', ['a'], 'a');
+
+const container = document.querySelector('main .container');
+container.appendChild(item1);
